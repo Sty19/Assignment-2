db9817a97cf643e79b4d7cee3c3cd49ef2e1dd8e
Shawn TImothy Tey Xi <115801000+Sty19@users.noreply.github.com>
2023-02-09 21:15:00 +0800

    Add files via upload

---
+++ Assignment 2/assets/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    window.addEventListener('scroll', function (e){
+        if (window.pageYOffset > 100) {
+            this.document.querySelector("header").classList.add('is-scrolling');
+        }
+    })
+}
